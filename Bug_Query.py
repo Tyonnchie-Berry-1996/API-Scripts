89f6fbc8b38606546ff1c41a1186aa6a78ab7189
Tyonnchie-Berry-1996 <g5fds626m7@privaterelay.appleid.com>
2025-11-21 18:05:06 -0500

    Update Bug_Query.py

--- Bug_Query.py
+++ Bug_Query.py
@@ -3,15 +3,13 @@ from BZ_API import api
 import urllib3
 import bugzilla
 import re
-from time import sleep
 import json
 import requests
 
-
 urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs="/etc/ssl/certs/ca-bundle.trust.crt")
 
-def product_finder():
 
+def product_finder():
     try:
         URL = "bugzilla.redhat.com/"
 
@@ -27,7 +25,6 @@ def product_finder():
         with open("id_advisories.json", 'r') as e:
             id_object = json.loads(e.read())
 
-        
         for x in range(0, id_object):
             product_id = id_object['ids'][x]
 
@@ -44,50 +41,34 @@ def product_finder():
 
 
 def bugz_finder():
+    try:
+        api_key = api
 
-    api_key = api
+        URL = "bugzilla.redhat.com/"
 
-    URL = "bugzilla.redhat.com/"
+        bzapi = bugzilla.Bugzilla(URL, api_key=api_key, force_rest=True)
+        assert bzapi.logged_in
 
-    bzapi = bugzilla.Bugzilla(URL, api_key=api_key, force_rest=True)
-    assert bzapi.logged_in
-    
-    query = bzapi.build_query(product="Fedora",
-                              component="kernel",
-                              status="NEW"
-                              )
+        query = bzapi.build_query(product="Fedora",
+                                  component="kernel",
+                                  limit=5
+                                  )
 
-    bugs = bzapi.query(query)
+        bugs = bzapi.query(query)
 
-    for i in range(0, len(bugs)):
-        matches = re.findall(r"[#]\d{1,10}", str(bugs[i]))
-        sleep(2)
+        print(f"Found {len(bugs)} bugs with in query")
 
-        bug_num = matches[0]
-        bug_id = bug_num[1:len(bug_num) - 1 + 1]
+        for i in range(0, len(bugs)):
+            matches = re.findall(r"[#]\d{1,10}", str(bugs[i]))
 
-        with open('bz-data.txt', 'a') as e:
-            e.write(str(bug_id) + "\n")
+            bug_num = matches[0]
+            bug_id = bug_num[1:len(bug_num) - 1 + 1]
+            link = "https://bugzilla.redhat.com/show_bug.cgi?id=" + bug_id
 
-        hoping = bzapi.build_query(dependson=bug_id)
-
-        dependson = bzapi.query(hoping)
-
-        print("\n" + f"Found {len(dependson)} bugs with in query")
-
-        for i in range(0, 1):
-
-            matched = re.findall(r"[#]\d{1,10}", str(dependson[i]))
-            print("\nRetrieving public tracking BZ")
-            sleep(3)
-
-            bug_numz= matched[0]
-            id_bug = bug_numz[1:len(bug_numz) - 1 + 1]
-            link = "https://bugzilla.redhat.com/show_bug.cgi?id=" + id_bug
+            print(bugs[i], "\n", link)
             
-            print(dependson[i],"\n",link)
-
-            sleep(3)
+    except IndexError:
+        pass
 
 if __name__ == '__main__':
     # product_finder()
