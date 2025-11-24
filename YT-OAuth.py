f3ba3da1a75af5959fd7d81048def8d5805186f7
Tyonnchie-Berry-1996 <g5fds626m7@privaterelay.appleid.com>
2025-11-23 22:56:55 -0500

    Update YT-OAuth.py

--- YT-OAuth.py
+++ YT-OAuth.py
@@ -9,7 +9,7 @@ with open('credentials.json', 'r') as f:
 
 flow = InstalledAppFlow.from_client_config(client_credentials, SCOPES)
 
-credentials = flow.run_local_server(access_type='offline')
+credentials = flow.run_local_server(access_type='offline', open_browser=False)
 
 print("Access token:", credentials.token)
 print("Refresh token:", credentials.refresh_token)
