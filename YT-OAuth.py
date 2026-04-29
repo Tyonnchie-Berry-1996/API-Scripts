daf7304dcdd4e0bc8df9897543cb927336fa2f3c
Tyonnchie-Berry-1996 <g5fds626m7@privaterelay.appleid.com>
2026-04-29 02:37:47 -0400

    Update YT-OAuth.py

--- YT-OAuth.py
+++ YT-OAuth.py
@@ -1,15 +1,36 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
-
+from pathlib import Path
 import json
 
 SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
+credentials_file = Path("credentials.json")
 
-with open('credentials.json', 'r') as f:
-    client_credentials = json.load(f)
+if credentials_file.exists():
+    with open(credentials_file, 'r') as f:
+        client_credentials = json.load(f)
+
+        client_id = input("\nPaste your client id").strip().lower()
+        client_credentials['installed']['client_id'] = client_id
+
+        client_secret = input("\nPaste your client secret").strip().lower()
+        client_credentials['installed']['client_secret'] = client_secret
+
+        project_id = input("\nPaste your project id").strip().lower()
+        client_credentials['installed']['project_id'] = project_id
+
+        refresh_token = input("\nPaste your refresh token").strip().lower()
+        client_credentials['installed']['refresh_token'] = refresh_token
+
+    with open(credentials_file, 'w') as e:
+        json.dump(client_credentials, e, indent=4)
+
+else:
+    client_credentials = {}
+
 
 flow = InstalledAppFlow.from_client_config(client_credentials, SCOPES)
 
-credentials = flow.run_local_server(access_type='offline', open_browser=False)
+credentials = flow.run_local_server(access_type='offline')
 
 print("Access token:", credentials.token)
 print("Refresh token:", credentials.refresh_token)
