4ec2d5aadba166298e714a26d4f71f2282e68e77
Tyonnchie-Berry-1996 <g5fds626m7@privaterelay.appleid.com>
2026-04-29 13:38:59 -0400

    Update Bug_Query.py

--- Bug_Query.py
+++ Bug_Query.py
@@ -79,7 +79,7 @@ def bugz_finder():
 
         bugs = bzapi.query(query)
 
-        print(f"\nFound {len(bugs)} bugs in query")
+        print(f"Found {len(bugs)} bugs in query")
 
         for i in range(0, len(bugs)):
             matches = re.findall(r"[#]\d{1,10}", str(bugs[i]))
