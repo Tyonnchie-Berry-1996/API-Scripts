ca1250fcb54faf122207ebd47ed930a7a425e098
Tyonnchie-Berry-1996 <g5fds626m7@privaterelay.appleid.com>
2026-05-04 23:46:04 -0400

    Update and rename Subscriber-list.py to Subscription-list.py

---
+++ Subscription-list.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+import json
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from pathlib import Path
+import readline
+
+SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
+credentials_file = Path("credentials.json")
+def auth_helper():
+    with open(credentials_file, 'r') as f:
+        readline.parse_and_bind("set editing-mode emacs")
+        readline.parse_and_bind('"\\e[D": backward-char')
+        readline.parse_and_bind('"\\e[C": forward-char')
+
+        client_credentials = json.load(f)
+
+        client_id = input("\nPaste your client id: ").strip()
+        client_credentials['installed']['client_id'] = client_id
+
+        client_secret = input("\nPaste your client secret: ").strip()
+        client_credentials['installed']['client_secret'] = client_secret
+
+        project_id = input("\nPaste your project id: ").strip()
+        client_credentials['installed']['project_id'] = project_id
+
+        refresh_token = input("\nPaste your refresh token: ").strip()
+        client_credentials['installed']['refresh_token'] = refresh_token
+
+    with open(credentials_file, 'w') as e:
+        json.dump(client_credentials, e, indent=4)
+
+    flow = InstalledAppFlow.from_client_secrets_file(credentials_file, SCOPES)
+    creds = flow.run_local_server(access_type='offline')
+
+    return build("youtube", "v3", credentials=creds)
+
+youtube = auth_helper()
+
+
+response = youtube.subscriptions().list(
+    part="snippet",
+    mySubscribers=True,
+    mine=True,
+    maxResults=50
+).execute()
+
+
+if "items" in response:
+    for item in response["items"]:
+        print(item)
+else:
+    print("No subscribers found.")
